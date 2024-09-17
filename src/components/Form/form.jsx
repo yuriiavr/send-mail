de5879f60b228fb91617583ab6cd1b9f2888e750
yuriiavr <94492796+yuriiavr@users.noreply.github.com>
2024-09-17 17:00:44 +0300

    2

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -1,41 +1,69 @@
-import css from './form.module.css'
+import css from "./form.module.css";
 
 const Form = () => {
   return (
-    <>
-      <h1>Email Mailing</h1>
-      <form action="">
+    <div className={css.formSection}>
+      <h1 className={css.title}>Email Mailing</h1>
+      <form className={css.form} action="">
         <div className={css.formCont}>
-            <label>
-              <span>Enter campaing name</span>
-              <input name='name' type="text" />
-            </label>
-            <label>
-              <span>Choice email domain</span>
-              <input name='email' type="text" />
-            </label>
-            <label>
-              <span>Select proxy </span>
-              <select name="proxy" id=""></select>
-            </label>
-            <div>
-                <label>
-                  <span>Select a group </span>
-                  <select name="group" id=""></select>
-                </label>
-                <label>
-                  <span>Select a amount </span>
-                  <input name='amount' type="text" />
-                </label>
+          <label>
+            <span>Enter campaing name</span>
+            <input name="name" type="text" />
+          </label>
+          <label>
+            <span>Choice email domain</span>
+            <div className={css.selectStyles}>
+              <img
+                className={css.selectArrow}
+                src={require("../../img/select-arrow.png")}
+                alt=""
+              />
+              <select name="email" id=""></select>
             </div>
+          </label>
+          <label>
+            <span>Select proxy </span>
+            <div className={css.selectStyles}>
+              <img
+                className={css.selectArrow}
+                src={require("../../img/select-arrow.png")}
+                alt=""
+              />
+              <select className={css.smallInp} name="proxy" id=""></select>
+            </div>
+          </label>
+          <div>
             <label>
-              <span>Choice email template</span>
-              <select name="template" id=""></select>
+              <span>Select a group </span>
+              <div className={css.selectStyles}>
+                <img
+                  className={css.selectArrow}
+                  src={require("../../img/select-arrow.png")}
+                  alt=""
+                />
+                <select className={css.smallInp} name="group" id=""></select>
+              </div>
             </label>
+            <label>
+              <span>Select a amount </span>
+              <input className={css.smallInp} name="amount" type="text" />
+            </label>
+          </div>
+          <label>
+            <span>Choice email template</span>
+            <div className={css.selectStyles}>
+              <img
+                className={css.selectArrow}
+                src={require("../../img/select-arrow.png")}
+                alt=""
+              />
+              <select name="proxy" id=""></select>
+            </div>
+          </label>
         </div>
-        <button type='submit'>Start</button>
+        <button className={css.startButton} type="submit">Start</button>
       </form>
-    </>
+    </div>
   );
 };
 

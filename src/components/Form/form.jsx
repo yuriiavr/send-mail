9e4442fc4837ae9c1b6f6d27e405601ed9eb952d
yuriiavr <94492796+yuriiavr@users.noreply.github.com>
2025-02-07 13:12:13 +0200

    321321

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import { baseUrl } from "../api/api";
 
 const Form = () => {
-
   const [groups, setGroups] = useState([]);
   const [templates, setTemplates] = useState([]);
   const [domains, setDomains] = useState([]);
@@ -17,46 +16,51 @@ const Form = () => {
     group: "",
     posted: "",
     templateName: "",
+    geo: "", 
   });
+
+  const countries = [
+    { code: "ES", name: "Spain" },
+    { code: "US", name: "United States" },
+    { code: "FR", name: "France" },
+    { code: "DE", name: "Germany" },
+    { code: "IT", name: "Italy" },
+    { code: "ZA", name: "South Africa" },
+  ];
 
   useEffect(() => {
     const fetchGroups = async () => {
       try {
-        const response = await axios.get(
-          `${baseUrl}`+`collections`
-        );
+        const response = await axios.get(`${baseUrl}collections`);
         setGroups(response.data.collections);
       } catch (error) {
         console.error("Помилка отримання груп: ", error);
       }
     };
-
     fetchGroups();
   }, []);
 
   useEffect(() => {
     const fetchTemplates = async () => {
       try {
-        const response = await axios.get(`${baseUrl}`+`templates`);
+        const response = await axios.get(`${baseUrl}templates`);
         setTemplates(response.data.templates);
       } catch (error) {
         console.error("Помилка отримання шаблонів: ", error);
       }
     };
-
     fetchTemplates();
   }, []);
 
   useEffect(() => {
     const fetchDomains = async () => {
       try {
-        const response = await axios.get(`${baseUrl}`+`domains`);
+        const response = await axios.get(`${baseUrl}domains`);
         setDomains(response.data.domains);
       } catch (error) {
         console.error("Помилка отримання доменів: ", error);
       }
     };
-
     fetchDomains();
   }, []);
 
@@ -71,22 +75,21 @@ const Form = () => {
     e.preventDefault();
     setIsSubmitting(true);
     try {
-      const response = await axios.post(
-        `${baseUrl}`+`senderMails/send`,
-        {
-          campaignName: formData.campaignName,
-          tempSubject: formData.tempSubject,
-          nameFrom: formData.nameFrom,
-          posted: formData.posted,
-          group: formData.group,
-          domainName: formData.domainName,
-          templateName: formData.templateName,
-        }
-      );
+      const response = await axios.post(`${baseUrl}senderMails/send`, {
+        campaignName: formData.campaignName,
+        tempSubject: formData.tempSubject,
+        nameFrom: formData.nameFrom,
+        posted: formData.posted,
+        group: formData.group,
+        domainName: formData.domainName,
+        templateName: formData.templateName,
+        geo: formData.geo, 
+      });
+
       console.log("Відправлено успішно:", response.data);
     } catch (error) {
       console.error("Помилка відправлення:", error);
-    }  finally {
+    } finally {
       setIsSubmitting(false);
     }
   };
@@ -97,7 +100,7 @@ const Form = () => {
       <form className={css.form} onSubmit={handleSubmit}>
         <div className={css.formCont}>
           <label>
-            <span>Enter campaing name</span>
+            <span>Enter campaign name</span>
             <input
               name="campaignName"
               type="text"
@@ -105,47 +108,66 @@ const Form = () => {
               onChange={handleChange}
             />
           </label>
-          <div style={{display: 'flex'}}>
+          <div style={{ display: "flex" }}>
             <label>
               <span>Enter email subject</span>
               <div className={css.selectStyles}>
-                <input name="tempSubject" value={formData.tempSubject} onChange={handleChange} type="text" autoComplete="disable" required/>
+                <input
+                  name="tempSubject"
+                  value={formData.tempSubject}
+                  onChange={handleChange}
+                  type="text"
+                  autoComplete="disable"
+                  required
+                />
               </div>
             </label>
             <label>
               <span>Enter sender name</span>
               <div className={css.selectStyles}>
-                <input type="text" required name="nameFrom" value={formData.nameFrom} onChange={handleChange}/>
+                <input
+                  type="text"
+                  required
+                  name="nameFrom"
+                  value={formData.nameFrom}
+                  onChange={handleChange}
+                />
               </div>
             </label>
           </div>
           <div>
             <label>
-              <span>Select a group </span>
+              <span>Select a country</span>
               <div className={css.selectStyles}>
                 <img
                   className={css.selectArrow}
                   src={require("../../img/select-arrow.png")}
                   alt=""
                 />
-                <select className={css.smallInp} name="group"
-                  value={formData.group}
-                  onChange={handleChange}>
-                    <option value="" disabled>Виберіть групу</option>
-                  {groups.map((group) => (
-                    <option key={group} value={group}>
-                      {group}
+                <select
+                  className={css.smallInp}
+                  name="geo"
+                  value={formData.geo}
+                  onChange={handleChange}
+                >
+                  <option value="" disabled>Select Country</option>
+                  {countries.map((country) => (
+                    <option key={country.code} value={country.code}>
+                      {country.name}
                     </option>
                   ))}
                 </select>
               </div>
             </label>
             <label>
-              <span>Select a amount </span>
-              <input className={css.smallInp} name="posted"
+              <span>Select amount</span>
+              <input
+                className={css.smallInp}
+                name="posted"
                 type="text"
                 value={formData.posted}
-                onChange={handleChange} />
+                onChange={handleChange}
+              />
             </label>
           </div>
           <label>
@@ -156,10 +178,12 @@ const Form = () => {
                 src={require("../../img/select-arrow.png")}
                 alt=""
               />
-              <select name="templateName"
+              <select
+                name="templateName"
                 value={formData.templateName}
-                onChange={handleChange}>
-                  <option value="" disabled>Виберіть шаблон</option>
+                onChange={handleChange}
+              >
+                <option value="" disabled>Selecet template</option>
                 {templates.map((template) => (
                   <option key={template} value={template}>
                     {template}
